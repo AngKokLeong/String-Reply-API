b8cbc1aeb18c26d5a4a9d08582d539949e65b1fc
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2024-03-04 14:32:14 +0000

    SRA-013 implement Message Processor and its unit tests qq

--- src/test/java/com/stringreplyservice/v2/replyservice/service/ReplyMessageProcessorTest.java
+++ src/test/java/com/stringreplyservice/v2/replyservice/service/ReplyMessageProcessorTest.java
@@ -1,10 +1,13 @@
 package com.stringreplyservice.v2.replyservice.service;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.stringreplyservice.v2.replyservice.exception.InvalidRequestException;
 
 
 @SpringBootTest
@@ -18,9 +21,35 @@ public class ReplyMessageProcessorTest {
         this.replyMessageProcessor = replyMessageProcessor;
     }
 
-    
+    @Test
+    public void testProcessMessageForInvalidRequestExceptionWithoutRule() throws Exception {
+        assertThrows(InvalidRequestException.class, () -> {
+            replyMessageProcessor.processMessage("-asdasd");
+        });
+    }
 
+    @Test
+    public void testProcessMessageForInvalidRequestExceptionWithoutMessage() throws Exception {
+        assertThrows(InvalidRequestException.class, () -> {
+            replyMessageProcessor.processMessage("13-");
+        });
+    }
 
+    @Test
+    public void testProcessMessageForInvalidRequestExceptionWithoutSeparator() throws Exception {
+        assertThrows(InvalidRequestException.class, () -> {
+            replyMessageProcessor.processMessage("13asdasd");
+        });
+    }
 
+    @Test
+    public void testProcessMessage() throws Exception {
+        String replyMessage = "12-asdasd";
+
+        String result = replyMessageProcessor.processMessage(replyMessage);
+
+        assertTrue(result.equals("c91c03ea6c46a86cbc019be3d71d0a1a"));
+
+    }
 
 }
